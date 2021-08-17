25ca04505518027a30cb45e2b22688fc2c4d3e73
jazzinlive <70212021+jazzinlive@users.noreply.github.com>
2021-08-17 20:55:25 +0700

    17-08-21

---
+++ is767_app/lib/main.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        //scaffoldBackgroundColor: Colors.grey.shade200,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Home'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Image cat = Image.asset('assets/POPCAT1.png');
+
+  Image cat1 = Image.asset('assets/POPCAT1.png');
+
+  Image cat2 = Image.asset('assets/POPCAT2.jpg');
+
+  void _incrementCounter() {
+    setState(() {
+      cat = cat2;
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      cat = cat1;
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.all(20),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: Offset(0, 1))
+                    ]),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    cat,
+                    Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: _decrementCounter,
+                  child:
+                      Text('Decrease', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(primary: Colors.red),
+                ),
+                ElevatedButton(
+                  onPressed: _incrementCounter,
+                  child:
+                      Text('Increase', style: TextStyle(color: Colors.white)),
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SubmitButton extends StatelessWidget {
+  final String buttonText;
+  SubmitButton(this.buttonText);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(
+        this.buttonText,
+        style: TextStyle(fontSize: 20),
+      ),
+      onPressed: () {
+        print('Pressing');
+      },
+    );
+  }
+}
